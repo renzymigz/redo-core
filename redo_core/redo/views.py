8b558d4ade0834368522c6a7107b88391c78bf02
Rence <itsrealflorence@gmail.com>
2025-10-04 21:18:05 +0800

    feature(redo_core): add tailwind

--- redo_core/redo/views.py
+++ redo_core/redo/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def landing_page(request):
+    return render(request, "landing.html")
+
 # Create your views here.
--- redo_core/redo_core/urls.py
+++ redo_core/redo_core/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from redo.views import landing_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", landing_page)
 ]
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
